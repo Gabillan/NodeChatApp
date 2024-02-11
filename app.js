9d2aa888cf88e34e5f2cb97806b343da165432cb
kiet <155219158+Gabillan@users.noreply.github.com>
2024-02-12 08:10:47 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// チャットメッセージを保存するJSONファイルのパス
 const messagesFilePath = __dirname + "/messages.json";
-const maxMessages = 20; // メッセージの最大数
+const maxMessages = 20;
 
-// ホームページの表示
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-// クライアントが接続した時の処理
 io.on("connection", (socket) => {
   console.log("クライアントが接続しました");
 
-  // チャットメッセージを読み込む関数
   function loadMessages() {
     try {
       const messagesData = fs.readFileSync(messagesFilePath, "utf8");
@@ -31,7 +27,6 @@ io.on("connection", (socket) => {
     }
   }
 
-  // チャットメッセージを保存する関数
   function saveMessages(messages) {
     try {
       const data = JSON.stringify(messages.slice(-maxMessages), null, 2);
@@ -41,32 +36,26 @@ io.on("connection", (socket) => {
     }
   }
 
-  // クライアントからメッセージを受信した時の処理
   socket.on("chat message", (msg) => {
     console.log("メッセージを受信しました: " + msg);
 
-    // 受信したメッセージを全てのクライアントに送信
     io.emit("chat message", msg);
 
-    // 新しいメッセージをJSONファイルに追加して保存
     const messages = loadMessages();
     messages.push(msg);
     saveMessages(messages);
   });
 
-  // クライアントが切断した時の処理
   socket.on("disconnect", () => {
     console.log("クライアントが切断しました");
   });
 
-  // 初期メッセージを読み込んでクライアントに送信
   const initialMessages = loadMessages();
   initialMessages.forEach((msg) => {
     socket.emit("chat message", msg);
   });
 });
 
-// サーバーを指定ポートで起動
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`サーバーがポート ${PORT} で起動しました`);
